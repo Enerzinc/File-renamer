a8144af357bffcdea835f6235600e544319b914f
JMC <135529552+Enerzinc@users.noreply.github.com>
2026-02-25 02:11:27 +0800

    adding validation

--- main.py
+++ main.py
@@ -4,8 +4,18 @@ class FileRenamer:
     def __init__(self, folder_path):
         self.folder_path = Path(folder_path)
         self.files_in_folder = [p for p in self.folder_path.iterdir() if p.is_file()]
+    def validate_folder(self):
+        if not self.folder_path.exists():
+            print("The specified folder does not exist.")
+            return False
+        if not self.folder_path.is_dir():
+            print("The specified path is not a folder.")
+            return False
+        return True
 
     def remove_dashes(self):
+      if not self.validate_folder():
+        return
       for file in self.files_in_folder:
         if file.is_file():
           new_name = file.name.replace("-", "")
